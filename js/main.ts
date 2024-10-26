5f0bb66a5fa7969e5fc41ac9fd488f16a4a1e521
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-10-26 15:43:52 +0200

    HW6: Completed

--- js/main.ts
+++ js/main.ts
@@ -1,7 +1,37 @@
-function sum(a: number, b: number): number {
-    return a + b;
+//I Version
+type TreeNode = {
+  value: number
+  child?: TreeNode | null
 }
 
-// Example usage:
-const result = sum(10, 10);
-console.log(result); // Output: 8
+const createTree = (num: number): TreeNode => {
+  if (num === 1) {
+    return { value: 1, child: null }
+  }
+
+  return {
+    value: num,
+    child: createTree(num - 1),
+  }
+}
+
+const res = createTree(3)
+console.log(res)
+
+//II Version
+// const createTree = (
+//   num: number,
+// ): { value: number; child?: ReturnType<typeof createTree> | null } => {
+//   const tree = { value: num }
+//
+//   if (num > 1) {
+//     tree.child = createTree(num - 1)
+//   } else if (num === 1) {
+//     tree.child = null
+//   }
+//
+//   return tree
+// }
+//
+// const res2 = createTree(3)
+// console.log(res2)
