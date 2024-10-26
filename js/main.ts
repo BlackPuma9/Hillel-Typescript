fa9bd87d8e89f215281a0d8161814b4d50ea0f5c
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-10-26 16:37:19 +0200

    HW7: completed

--- js/main.ts
+++ js/main.ts
@@ -1,7 +1,26 @@
-function sum(a: number, b: number): number {
-    return a + b;
+//I version
+
+const reverseArray = (
+  arr: Array<number>,
+  start: number = 0,
+  end: number = arr.length - 1,
+): Array<number> => {
+  if (start >= end) return arr
+
+  const temp = arr[start]
+  arr[start] = arr[end]
+  arr[end] = temp
+
+  return reverseArray(arr, start + 1, end - 1)
 }
 
-// Example usage:
-const result = sum(10, 10);
-console.log(result); // Output: 8
+const arr = [1, 2, 3]
+const reversedArr = reverseArray(arr)
+console.log(reversedArr)
+console.log(reversedArr === arr) // true
+
+//II version
+
+// const reverseArray = (arr: Array<number>): Array<number> => {
+//   return arr.reverse()
+// }
